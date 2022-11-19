0c7ae9be463a32b0e06e6c4ac029abdb50d37930
QWQ-cat <80164433+QWQ-cat@users.noreply.github.com>
2022-11-19 22:58:56 +0800

    Delete python_reptile.py

--- python_reptile.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-web = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html', cookies = {'over18':'1'})
-soup = BeautifulSoup(web.text , 'html5lib')
-soup.remove
-print(soup.title)

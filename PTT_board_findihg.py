c54e3f490f20b4942d0496cc8da2da895d31191b
QWQ-cat <80164433+QWQ-cat@users.noreply.github.com>
2022-11-25 22:47:51 +0800

    Update PTT_board_findihg.py

--- PTT_board_findihg.py
+++ PTT_board_findihg.py
@@ -1,22 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = 'https://www.ptt.cc/bbs/'
-web = requests.get('https://www.ptt.cc/bbs/index.html')
-soup = BeautifulSoup(web.text, 'html5lib')
-board_name = soup.find_all('div', class_='board-name')
-hot = soup.find_all('span', class_='hl f6')
-board_class = soup.find_all('div', class_='board-class')
-board_title = soup.find_all('div', class_='board-title')
-'''
-print(board_name, end=' ')
-print(board_class, end='*')
-print(board_title, end='~')
-'''
-# https://ithelp.ithome.com.tw/articles/10228998?sc=pt
-for i in board_name:
-    if i.find('board-name') != None:
-        print(board_name[i], ' ', hot[i], ' ',
-              board_class[i], ' ', board_title[i], '\n')
-        print(url+board_name[i])
+hotpage = requests.get('https://www.ptt.cc/bbs/hotboards.html')
+soup = BeautifulSoup(hotpage.text, 'html5lib')
+# print(soup)
+board_find = soup.find_all('a', class_='board')
+for board in board_find:
+    header_name = board.find('div', class_='board-name')
+    print("看板名稱：", header_name.text)
+    header_page = board.select('span')[0]
+    print("看板分類文章數：", header_page.text)
+    header_classes = board.find('div', class_='board-class')
+    print("看板分類：", header_classes.text)
+    header_title = board.select('div.board-title')[0]
+    print("看板標題：", header_title.text)
+    header_url = 'https://www.ptt.cc' + board['href']
+    print("看板網址：" + header_url)
+    print('\n\n')
